6004d4162ab9bf32a78e9371b011470c3712588e
alexcoder04 <alexcoder04@protonmail.com>
2025-02-09 11:33:24 +0100

    generate graphs for different stations

--- graphs/dataloader.py
+++ graphs/dataloader.py
@@ -6,10 +6,15 @@ def load(api_host: str, station: str, hours: int = 24):
     ts_end = datetime.now()
     ts_start = ts_end - timedelta(hours=hours)
 
-    INPUT_URL = f"http://{api_host}/api/data?station={station}&time_from={ts_start.strftime('%Y-%m-%d_%H:%M:%S')}&time_to={ts_end.strftime('%Y-%m-%d_%H:%M:%S')}"
+    input_url = f"http://{api_host}/api/data?station={station}&time_from={ts_start.strftime('%Y-%m-%d_%H:%M:%S')}&time_to={ts_end.strftime('%Y-%m-%d_%H:%M:%S')}"
 
-    data = pd.read_csv(INPUT_URL)
+    data = pd.read_csv(input_url)
     data["timestamp"] = pd.to_datetime(data["timestamp"], format='%Y-%m-%d_%H:%M:%S')
 
     return data
+
+
+def get_stations(api_host: str):
+    input_url = f"http://{api_host}/api/stations"
+    return pd.read_csv(input_url)["id"].tolist()
 
--- graphs/main.py
+++ graphs/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from dataloader import load
+from dataloader import load, get_stations
 from graph import generate
 from preprocessor import for_1d, for_7d
 
@@ -9,7 +9,7 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate athocs graphs")
-    parser.add_argument("--station", type=str, required=True, help="ID of the station")
+    parser.add_argument("--station", type=str, default="all", help="ID of the station")
     parser.add_argument("--interval", type=str, choices=["1d", "7d"], required=True, help="Time interval (valid values: 1d, 7d)")
     parser.add_argument("--output-dir", type=str, default=".", help="Directory to save the graphs into")
     parser.add_argument("--api-host", type=str, default="192.168.0.86:1111", help="Hostname/IP and port of the API")
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     title_interval = None
     preprocess = None
 
+    # no match-case for now because of ancient packages on debian
     if args.interval == "1d":
         if args.average: hours = 7 * 24
         else: hours = 24
@@ -35,27 +36,22 @@ if __name__ == "__main__":
         title_interval = "7 Days (smoothed)"
         preprocess = for_7d
 
-# we can't use this for now because of debians ancient packages
-#    match args.interval:
-#        case "1d":
-#            if args.average: hours = 7 * 24
-#            else: hours = 24
-#            xticks = 1
-#            title_interval = "24 Hours"
-#            preprocess = for_1d
-#        case "7d":
-#            hours = 7 * 24
-#            xticks = 4
-#            title_interval = "7 Days (smoothed)"
-#            preprocess = for_7d
+    stations = [args.station]
+    if args.station == "all":
+        try:
+            stations = get_stations(args.api_host)
+        except Exception as e:
+            print("Loading stations list failed")
+            print(e)
 
-    try:
-        data = load(args.api_host, args.station, hours=hours)
-    except Exception as e:
-        print("Loading the data failed")
-        print(e)
+    for st in stations:
+        try:
+            data = load(args.api_host, st, hours=hours)
+        except Exception as e:
+            print("Loading the data failed")
+            print(e)
 
-    data, avg_data = preprocess(data, average=args.average)
+        data, avg_data = preprocess(data, average=args.average)
 
-    generate(data, avg_data, f"{args.output_dir}/{args.interval}{'' if not args.average else '-avg'}.png", f"Temperature, Humidity and Pressure in the Last {title_interval}{'' if not args.average else ' (with average of last 7 days)'}", xticks)
+        generate(data, avg_data, f"{args.output_dir}/{st}-{args.interval}{'' if not args.average else '-avg'}.png", f"Temperature, Humidity and Pressure in the Last {title_interval}{'' if not args.average else ' (with average of last 7 days)'} at {st}", xticks)
 
